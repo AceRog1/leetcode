0c8ef0fc7fc2915aae50e3ce3f99c9149f193e8b
acerog <andresrojas72727264@gmail.com>
2024-09-01 16:54:21 -0500

    Terminado y Aceptado

--- 71/main.cpp
+++ 71/main.cpp
@@ -7,7 +7,7 @@ using namespace std;
 class Solution {
 public:
     string simplifyPath(string path) {
-        string cd = "CD", ud = "UD", p;
+        string cd = "CD", ud = "UD", p, dotsList;
         string resultPath;
         stack<string> stk;
         size_t dots = 0;
@@ -19,14 +19,24 @@ public:
             if (path[i] == '/' || i == path.size()){
                 if(dots != 0){
                     if (dots == 1){
-                        p = cd;
-                    } else if (dots == 2){
-                        p = ud;
-                    } else {
-                        while (dots != 0){
-                            p += '.';
-                            dots--;
+                        if (path[i-2] != '/'){
+                            p += dotsList;
+                            dotsList.clear();
+                        } else {
+                            p = cd;
+                            dotsList.clear();
                         }
+                    } else if (dots == 2){
+                        if (path[i-3] != '/'){
+                            p += dotsList;
+                            dotsList.clear();
+                        } else {
+                            p = ud;
+                            dotsList.clear();
+                        }
+                    } else {
+                        p += dotsList;
+                        dotsList.clear();
                     }
                     dots = 0;
                 }
@@ -36,8 +46,11 @@ public:
                 }
             } else if (path[i] == '.'){
                 dots++;
+                dotsList += ".";
             } else {
-                p = p + path[i];
+                p += dotsList + path[i];
+                dots = 0;
+                dotsList.clear();
             }
         }
 
@@ -52,15 +65,12 @@ public:
                 udCounter++;
                 stk.pop();
             } else {
-                switch(udCounter){
-                    case 0:
-                        resultPath = "/" + stk.top() + resultPath;
-                        stk.pop();
-                        break;
-                    default:
-                        stk.pop();
-                        udCounter--;
-                        break;
+                if(udCounter == 0){
+                    resultPath = "/" + stk.top() + resultPath;
+                    stk.pop();
+                } else {
+                    stk.pop();
+                    udCounter--;
                 }
             }
         }
@@ -75,27 +85,28 @@ public:
 int main(){
     Solution sol;
 
-    //string path = ".";
+    string path1 = "/home/"; // /home
+    string path2 = "/home//foo/"; // /home/foo
+    string path3 = "/home/user/Documents/../Pictures"; // /home/user/Pictures
+    string path4 = "/../"; // /
+    string path5 = "/.../a/../b/c/../d/./"; // /.../b/d
+    string extraPath1 = "/a/./b/../../c/"; // /c
+    string extraPath2 = "/a//b////c/d//././/.."; // /a/b/c
+    string extraPath3 = "/"; // /
+    string extraPath4 = "/..hidden"; // /..hidden
+    string extraPath5 = "/hello../world"; // /hello../world
 
-    //cout << sol.simplifyPath(path);
-
-    //string path1 = "/home/......../no"; // /home
-    //string path2 = "/home//foo/"; // /home/foo
-    //string path3 = "/home/user/Documents/../Pictures"; // /home/user/Pictures
-    string path4 = "/../"; // /../
-    //string path5 = "/.../a/../b/c/../d/./"; // /.../b/d
-    //string extraPath1 = "/a/./b/../../c/"; // /c
-    //string extraPath2 = "/a//b////c/d//././/.."; // /a/b/c
-    //string extraPath3 = "/"; // /
-//
-    //cout << sol.simplifyPath(path1) << "\n";
-    //cout << sol.simplifyPath(path2) << "\n";
-    //cout << sol.simplifyPath(path3) << "\n";
+    cout << sol.simplifyPath(path1) << "\n";
+    cout << sol.simplifyPath(path2) << "\n";
+    cout << sol.simplifyPath(path3) << "\n";
     cout << sol.simplifyPath(path4) << "\n";
-    //cout << sol.simplifyPath(path5) << "\n";
-    //cout << sol.simplifyPath(extraPath1) << "\n";
-    //cout << sol.simplifyPath(extraPath2) << "\n";
-    //cout << sol.simplifyPath(extraPath3) << "\n";
+    cout << sol.simplifyPath(path5) << "\n";
+    cout << sol.simplifyPath(extraPath1) << "\n";
+    cout << sol.simplifyPath(extraPath2) << "\n";
+    cout << sol.simplifyPath(extraPath3) << "\n";
+    cout << sol.simplifyPath(extraPath4) << "\n";
+    cout << sol.simplifyPath(extraPath5) << "\n";
+
 
     return 0;
 }
