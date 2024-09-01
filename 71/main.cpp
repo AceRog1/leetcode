569fda295ff1ff739010765fd298e72e0b4be3c1
acerog <andresrojas72727264@gmail.com>
2024-09-01 01:10:29 -0500

    version del ejercicio 71 con Tokens (en progreso)

--- 71/main.cpp
+++ 71/main.cpp
@@ -7,61 +7,49 @@ using namespace std;
 class Solution {
 public:
     string simplifyPath(string path) {
-        string s;
-        if (path[0] != '/'){return s;}
-        stack<char> stk;
-        size_t activationTimes = 0, dots = 0;
-        bool activate = false;
-        for (int i = path.size()-1; i > -1; i--){
-            if (!activate){
-                if (path[i] == '.')
-                    dots++;
-                if (path[i] == '/') {
-                    if (!stk.empty()){
-                        if (stk.top() != '/')
-                            stk.push(path[i]);
-                    }
-                    switch(dots){
-                        case 1:
-                            stk.pop();
-                            stk.pop();
-                            dots = 0;
-                            break;
-                        case 2:
-                            if (i == 0){
-                                char ram = stk.top();
-                                stk.pop();
-                                stk.pop();
-                                stk.pop();
-                                stk.push(ram);
-                            } else {
-                                stk.pop();
-                                stk.pop();
-                                stk.pop();
+        string cd = "CD", ud = "UD", p;
+        string resultPath;
+        stack<string> stk;
+        size_t dots = 0;
+
+        for(int i = path.size()-1; i >= 0; i--){ // Tokeniso el Path
+            switch(path[i]){
+                case '/':
+                    if(dots != 0){
+                        if (dots == 1){
+                            p = cd;
+                        } else if (dots == 2){
+                            p = ud;
+                        } else {
+                            while (dots != 0){
+                                p += '.';
+                                dots--;
                             }
-                            dots = 0;
-                            activate = true;
-                            break;
-                        default:
-                            break;
+                        }
+                        dots = 0;
                     }
-                } else {
-                    stk.push(path[i]);
-                }
-            } else {
-                if (path[i] == '/') {
-                    activate = false;
-                    //stk.push(path[i]);
-                }
+                    if (!p.empty()){
+                        stk.push(p);
+                        p.clear();
+                    }
+                    break;
+                case '.': dots++; break;
+                default:
+                    p = path[i] + p;
+                    break;
             }
         }
 
-        while(!stk.empty()){
-            s += stk.top();
-            stk.pop();
-        }
+        if (stk.empty()) // Si no entuentro elementos en el stack, muestro la ruta raiz.
+            return "/";
 
-        return s;
+        while(!stk.empty()){
+            cout << "TOK[" << stk.top() << "]" << "\n"; // Identifica todos los Tokens en el Path
+            resultPath += "/" + stk.top();
+            stk.pop();
+        } /// TODO falta identificar cada Token en el Stack y realizar operaciones con ellos.
+
+        return resultPath;
     }
 };
 
@@ -72,23 +60,23 @@ int main(){
 
     //cout << sol.simplifyPath(path);
 
-    //string path1 = "/home/"; // /home
+    //string path1 = "/home/......../no"; // /home
     //string path2 = "/home//foo/"; // /home/foo
     //string path3 = "/home/user/Documents/../Pictures"; // /home/user/Pictures
     //string path4 = "/../"; // /../
     //string path5 = "/.../a/../b/c/../d/./"; // /.../b/d
-    string extraPath1 = "/a/./b/../../c/"; // /c ///TODO
+    //string extraPath1 = "/a/./b/../../c/"; // /c ///TODO
     //string extraPath2 = "/a//b////c/d//././/.."; // /a/b/c ///TODO
-    //string extraPath3 = "/"; // / ///TODO
+    string extraPath3 = "/"; // / ///TODO
 //
     //cout << sol.simplifyPath(path1) << "\n";
     //cout << sol.simplifyPath(path2) << "\n";
     //cout << sol.simplifyPath(path3) << "\n";
     //cout << sol.simplifyPath(path4) << "\n";
     //cout << sol.simplifyPath(path5) << "\n";
-    cout << sol.simplifyPath(extraPath1) << "\n";
+    //cout << sol.simplifyPath(extraPath1) << "\n";
     //cout << sol.simplifyPath(extraPath2) << "\n";
-    //cout << sol.simplifyPath(extraPath3) << "\n";
+    cout << sol.simplifyPath(extraPath3) << "\n";
 
     return 0;
 }
