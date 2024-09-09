6ae0014035c37d760acb3bf5e09b7439446be5e7
acerog <andresrojas72727264@gmail.com>
2024-09-09 07:36:09 -0500

    adicion de la clase del ejercicio

--- 145/main.cpp
+++ 145/main.cpp
@@ -1,4 +1,35 @@
 #include <iostream>
+#include <vector>
+
+using namespace std;
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+
+struct TreeNode {
+    int val; // valor
+    TreeNode *left; // nodo izquierdo
+    TreeNode *right; // nodo derecho
+    TreeNode() : val(0), left(nullptr), right(nullptr) {} // constructor por default
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {} // constructor con variable inicializada
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {} // constructor con todas las variables inicializadas
+};
+
+class Solution {
+public:
+    vector<int> postorderTraversal(TreeNode* root) {
+        
+    }
+};
 
 int main() {
     std::cout << "Hello, World!" << std::endl;
