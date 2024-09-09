e4b65f00be01d897d46316edab6d571029fe361c
acerog <andresrojas72727264@gmail.com>
2024-09-09 07:50:28 -0500

    corrio el codigo eliminamos comentarios

--- 145/main.cpp
+++ 145/main.cpp
@@ -4,24 +4,16 @@
 using namespace std;
 
 /**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
+ * 
  */
 
 struct TreeNode {
-    int val; // valor
-    TreeNode *left; // nodo izquierdo
-    TreeNode *right; // nodo derecho
-    TreeNode() : val(0), left(nullptr), right(nullptr) {} // constructor por default
-    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {} // constructor con variable inicializada
-    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {} // constructor con todas las variables inicializadas
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
 };
 
 class Solution {
