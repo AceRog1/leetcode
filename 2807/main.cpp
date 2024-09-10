00917dc8cd281fd4ec5c5c6f4fc33614a72f35e5
acerog <andresrojas72727264@gmail.com>
2024-09-10 15:03:43 -0500

    Agregamos nombre y eliminamos comentarios

--- 2807/main.cpp
+++ 2807/main.cpp
@@ -1,16 +1,13 @@
+//
+// Created by acerog on 10/09/24.
+//
+
 #include <iostream>
 
 using namespace std;
 
 /**
- * Definition for singly-linked list.
- * struct ListNode {
- *     int val;
- *     ListNode *next;
- *     ListNode() : val(0), next(nullptr) {}
- *     ListNode(int x) : val(x), next(nullptr) {}
- *     ListNode(int x, ListNode *next) : val(x), next(next) {}
- * };
+ * 
  */
 
 struct ListNode {
