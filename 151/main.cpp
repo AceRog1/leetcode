6381dd3702921af0a3ae9d5f1de9bcab1f1075e6
acerog <andresrojas72727264@gmail.com>
2024-09-15 12:58:48 -0500

    Primer intento de implementacion con tokens

--- 151/main.cpp
+++ 151/main.cpp
@@ -1,12 +1,37 @@
 #include <iostream>
 #include <string>
+#include <stack>
+#include <utility>
 
 using namespace std;
 
 class Solution {
 public:
     string reverseWords(string s) {
+        string reverse;
+        stack<string> tokens;
+        tokenization(std::move(s), tokens);
 
+        while(!tokens.empty()){
+            if (tokens.size() == 1){ reverse += tokens.top(); }
+            else { reverse += tokens.top() + " "; }
+            tokens.pop();
+        }
+
+        return reverse;
+    }
+    static void tokenization(const string s, stack<string>& tokens){
+        string token;
+        for (char i : s){
+            if (i != ' '){
+                token += i;
+            } else {
+                if (!token.empty()){
+                    tokens.push(token);
+                }
+                token.clear();
+            }
+        }
     }
 };
 
