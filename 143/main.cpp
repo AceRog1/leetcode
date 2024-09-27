eb8ad265daa8ffde8cd577149806ae83b8fa583c
acerog <andresrojas72727264@gmail.com>
2024-09-27 10:36:51 -0500

    primer intento ejercicio 143

--- 143/main.cpp
+++ 143/main.cpp
@@ -1,4 +1,6 @@
 #include <iostream>
+#include <stack>
+#include <queue>
 
 using namespace std;
 
@@ -24,11 +26,84 @@ struct ListNode {
 class Solution {
 public:
     void reorderList(ListNode* head) {
-
+        size_t size = 0;
+        ListNode *tempSize = head;
+        while(tempSize != nullptr){
+            size++;
+            tempSize = tempSize->next;
+        }
+        size_t half = size/2;
+        ListNode *tempFront = head;
+        ListNode *tempBack = head;
+        size_t cont = 0;
+        stack<int> stk;
+        queue<int> qeu;
+        while (tempBack != nullptr){
+            if (cont < half){
+                qeu.push(tempFront->val);
+                tempFront = tempFront->next;
+                tempBack = tempBack->next;
+            } else {
+                stk.push(tempBack->val);
+                tempBack = tempBack->next;
+            }
+            cont++;
+        }
+        while (head != nullptr){
+            ListNode *temp = head;
+            head = head->next;
+            delete temp;
+        }
+        ListNode *temp = head;
+        while(!stk.empty() || !qeu.empty()){
+            if (!qeu.empty()){
+                auto *newNode = new ListNode(qeu.front());
+                if (temp == nullptr && head == nullptr){
+                    head = newNode;
+                    temp = newNode;
+                } else {
+                    temp->next = newNode;
+                    temp = newNode;
+                }
+                qeu.pop();
+            }
+            if (!stk.empty()){
+                auto *newNode = new ListNode(stk.top());
+                temp->next = newNode;
+                temp = newNode;
+                stk.pop();
+            }
+        }
     }
 };
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+
+    Solution sol;
+
+    auto *head = new ListNode(1);
+    auto *node2 = new ListNode(2);
+    auto *node3 = new ListNode(3);
+    auto *node4 = new ListNode(4);
+    //ListNode *node5 = new ListNode(5);
+    //ListNode *node6 = new ListNode(6);
+
+    head->next = node2;
+    node2->next = node3;
+    node3->next = node4;
+
+    sol.reorderList(head);
+
+    ListNode *temp = head;
+    while (temp != nullptr){
+        cout << temp->val << " ";
+        temp = temp->next;
+    }
+
+    delete head;
+    delete node2;
+    delete node3;
+    delete node4;
+
     return 0;
 }
